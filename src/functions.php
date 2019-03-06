e74bebca7b8d749d800cb5bc9f438530cf267717
Beno!t POLASZEK <bpolaszek@gmail.com>
2019-03-06 12:13:43 +0100

    Removed StringCaster class in favor of an anonymous class because it is too much internal

--- src/functions.php
+++ src/functions.php
@@ -65,11 +65,99 @@ function current_location(UriFactoryInterface $factory = null): UriInterface
 
 /**
  * @param $value
- * @return StringCaster
+ * @return object
  */
-function cast($value): StringCaster
+function cast($value)
 {
-    return new StringCaster($value);
+    return new class($value)
+    {
+        private $value;
+
+        public function __construct(?string $value)
+        {
+            $this->value = $value;
+        }
+
+        /**
+         * @return int
+         */
+        public function asInt(): int
+        {
+            return (int) $this->value;
+        }
+
+        /**
+         * @return int|null
+         */
+        public function asIntOrNull(): ?int
+        {
+            if (0 === strlen((string) $this->value)) {
+                return null;
+            }
+
+            return (int) $this->value;
+        }
+
+        /**
+         * @return float
+         */
+        public function asFloat(): float
+        {
+            return (float) $this->value;
+        }
+
+        /**
+         * @return float|null
+         */
+        public function asFloatOrNull(): ?float
+        {
+            if (0 === strlen((string) $this->value)) {
+                return null;
+            }
+
+            return (float) $this->value;
+        }
+
+        /**
+         * @return string
+         */
+        public function asString(): string
+        {
+            return (string) $this->value;
+        }
+
+        /**
+         * @return string|null
+         */
+        public function asStringOrNull(): ?string
+        {
+            if (0 === strlen((string) $this->value)) {
+                return null;
+            }
+
+            return (string) $this->value;
+        }
+
+        /**
+         * @return bool
+         */
+        public function asBool(): bool
+        {
+            return filter_var($this->value, FILTER_VALIDATE_BOOLEAN);
+        }
+
+        /**
+         * @return bool|null
+         */
+        public function asBoolOrNull(): ?bool
+        {
+            if (0 === strlen((string) $this->value)) {
+                return null;
+            }
+
+            return filter_var($this->value, FILTER_VALIDATE_BOOLEAN);
+        }
+    };
 }
 
 /**
